a8b7fec36188516432d21dfefd746c14f8348031
jhashekhar <shekhar09jha@gmail.com>
2020-08-16 02:18:55 +0530

    update parser

--- src/main.hs
+++ src/main.hs
@@ -1,13 +1,62 @@
 import Text.ParserCombinators.Parsec hiding (spaces)
 import System.Environment
+import Control.Monad
+
 
 symbol :: Parser Char
 symbol = oneOf "!#$%&|*+-/:<=>?@^_~"
 
+data LispVal = Atom String
+             | List [LispVal]
+             | DottedList [LispVal] LispVal
+             | Number Integer
+             | String String
+             | Bool Bool
+
+
+-- | function to call our parser and handle any possible errors
 readExpr :: String -> String
-readExpr input = case parse symbol "lisp" input of
+readExpr input = case parse parseExpr "lisp" input of
     Left err -> "No match: " ++ show err
-    Right val -> "Found value"
+    Right _ -> "Found value"
+
+
+-- | function to handle spaces in input string
+spaces :: Parser ()
+spaces = skipMany1 space
+
+
+parseString :: Parser LispVal
+parseString = do
+                char '"'
+                x <- many (noneOf "\"")
+                char '"'
+                return $ String x
+
+-- | Atom parser
+parseAtom :: Parser LispVal
+parseAtom = do
+              first <- letter <|> symbol
+              rest <- many (letter <|> digit <|> symbol)
+              let atom = first:rest
+              return $ case atom of 
+                         "#t" -> Bool True
+                         "#f" -> Bool False
+                         _    -> Atom atom
+
+
+-- | Int parser
+parseNumber :: Parser LispVal
+parseNumber = liftM (Number . read) $ many1 digit
+
+
+
+parseExpr :: Parser LispVal
+parseExpr = parseAtom
+          <|> parseString
+          <|> parseNumber
+
+
 
 main :: IO ()
 main = do
